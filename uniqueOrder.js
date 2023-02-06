0abcb6890cdc3cc8aedbc787076e02bd37d88ae1
“Anthony <antkameka@gmail.com>
2023-02-06 11:44:34 -0500

    uniqueOrder

---
+++ uniqueOrder.js
@@ -0,0 +1,39 @@
+/*
+Implement the function unique_in_order which takes as argument a sequence and 
+returns a list of items without any elements with the same value 
+next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+
+
+------
+Input = array or string
+Output = array without repeating characters
+
+steps
+make empty array
+iterate through the input
+if input[i] is !== input[i+1] then push that result
+
+
+
+*/
+
+
+var uniqueInOrder=function(iterable){
+    //your code here - remember iterable can be a string or an array
+    let result = [];
+    
+    for (let i = 0; i < iterable.length; i++) {
+        if(iterable[i] !== iterable[i+1]) {
+        result.push(iterable[i])
+        }
+    }
+    
+    return result
+}
